35247ebb7063082fe147ee63a0f778c27a6c8280
Kevin <kevin.fal360@gmail.com>
2018-10-16 07:14:28 -0400

    added demo build folder

---
+++ Demo Build/django_project/blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+def save(self, *args, **kwargs):
+  if self.clean_name == "":
+    self.clean_name = self.name
+  super(Game, self).save(*args, **kwargs)
+ 
